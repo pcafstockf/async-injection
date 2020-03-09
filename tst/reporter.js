f60e0a86050e8b41498767c391365b558b9e7275
pcafstockf <pcafstockf@github.com>
2020-03-09 14:32:46 -0700

    API Docs

--- tst/reporter.js
+++
@@ -1,5 +0,0 @@
-// jasmine-ts-console-reporter doesn't seem to work for us, and this one reports line numbers in .ts files just fine.
-const JasmineConsoleReporter = require("jasmine-console-reporter");
-// Clear default console reporter
-jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(new JasmineConsoleReporter());
