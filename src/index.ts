979ba60d8625da3bcabfd38733953a315d7fe92d
pcafstockf <pcafstockf@github.com>
2020-04-09 10:08:00 -0700

    Allow abstract class constructor as binding id.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {Injectable, Inject, PostConstruct, Optional} from './decorators';
-export {InjectableId, Injector} from './injector';
+export {InjectableId, ClassConstructor, Injector} from './injector';
 export {Binder} from './binder';
 export {Container} from './container';
